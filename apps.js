b3ae70d3ad80ee4c457167ad1fe538f93be1d8ed
JOAO VICTOR DA SILVA GUILHERME <joao.vsguilherme@senacsp.edu.br>
2024-11-07 22:16:24 -0300

    controle da lampada

--- apps.js
+++ apps.js
@@ -3,13 +3,38 @@
  * @author João Victor
  */
 
-function quebrar(){
-    document.getElementById('lamp').src="img/broken.jpg"
+//variaveis de apoio logico
+let chave = false // o interruptor inicia desligado
+let lampada = true // a lampada nao esta quebrada 
+
+function quebrar() {
+    if (lampada === true){
+        document.getElementById('lamp').src = "img/broken.jpg"
     //reproduzindo um arquivo de audio do JS
     //passo 1: copiar o arquivo de audio para o projeto
     //passo 2: Usar a classe Audio(biblioteca interna do Js)
 
-let som = new Audio()
-som.src = "sound/glassbreaking.wav"
-som.play()
+    let som = new Audio()
+    som.src = "sound/glassbreaking.wav"
+    som.play()
+    // apoio a logica para o Js indetificar a lampada quebrada
+    lampada = false
+    
+    }
+}
+
+function onoff() {
+
+    if (chave === false && lampada === true) {
+        // ligar a chave 
+        document.getElementById('interruptor').src = "img/swon.png"
+        chave = true // o Js agora sabe que a chave esta ligada 
+        document.getElementById('lamp').src = "img/on.jpg"
+        //acender a lampada 
+    } else {
+        document.getElementById('interruptor').src = "img/swoff.png"
+        chave = false// o Js agora sabe que a chave esta desligada
+        document.getElementById('lamp').src = "img/off.jpg"
+        //apagar a lampada 
+    }
 }
